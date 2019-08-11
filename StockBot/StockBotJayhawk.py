689866ab7abb3af3e668fb469ec321af4c30ea89
Gam8it <32150386+Gam8itG4mes@users.noreply.github.com>
2019-08-10 20:46:51 -0700

    Now functional and compatible w/ most recent version of discord.py

--- StockBot/StockBotJayhawk.py
+++ StockBot/StockBotJayhawk.py
@@ -27,76 +27,82 @@ async def eight_ball(context):
 		'Too hard to tell',
 		'Definitely'
 	]
-	await client.say(random.choice(possible_responses) + ', ' +
+	await context.send(random.choice(possible_responses) + ', ' +
 		context.message.author.mention)
 '''
 @client.command()
 async def square(number):
 	sq_val = int(number) * int(number)
-	await client.say(str(number) + ' squared is ' + str(sq_val))
+	await client.send(str(number) + ' squared is ' + str(sq_val))
 '''
-
+"""
 @client.event
 async def on_ready():
 	await client.change_presence(game = Game(name='with Humans'))
 	print('logged in as ' + client.user.name)
+"""
 
 @client.command()
 async def bitcoin():
 	url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
 	response = requests.get(url)
 	value = response.json()['bpi']['USD']['rate']
-	await client.say("Bitcoin price is $ " + value)
+	await client.send("Bitcoin price is $ " + value)
 
 
 @client.command(description =  'Returns the stock information for the given stock of the provided NYSE abbreviation',
 				brief = 'Provides stock market information',
 				aliases = ['stocks'])
-async def stock(symbol):
-	if symbol == 'SPX':
-		quote_page_spx = ('https://www.bloomberg.com/quote/SPX:IND')
+async def stock(self, symbol):
+	#symbol = symbol.upper()
+	if symbol == 'SPX' or symbol.upper() == "S&P":
+		quote_page_spx = ('https://www.reuters.com/finance/markets/index/.SPX')
 		page_spx = urllib.request.urlopen(quote_page_spx)
 		soup = BeautifulSoup(page_spx, 'html.parser')
 
-		name_box_spx = soup.find('h1', attrs={'class':'name'})
-		price_box_spx = soup.find('div', attrs={'class':'price'})
-		change_box_spx = soup.find('div', attrs={'class':'change-container'})
+		name_box_spx = soup.find('div', {'id':'sectionTitle'})
+		price_box_spx = soup.find('div', {'class':'dataHeader'})
+		#change_box_spx = soup.find('div', {'class':'change-container'})
 
-		await client.say(name_box_spx.text.strip())
-		await client.say('$' + price_box_spx.text)
-		await client.say(change_box_spx.text.strip())
-		await client.say(quote_page_spx)
+		await self.send(name_box_spx.text.strip())
+		await self.send('$' + price_box_spx.text.strip())
+		#await self.send(change_box_spx.text.strip())
+		#await self.send(quote_page_spx)
 
 	elif symbol == "INDU" or symbol == "DJIA":
-		quote_page_dja = ('https://www.bloomberg.com/quote/INDU:IND')
+		quote_page_dja = ('https://www.reuters.com/finance/markets/index/.DJI')
 		page_dja = urllib.request.urlopen(quote_page_dja)
 		soup = BeautifulSoup(page_dja, 'html.parser')
 
-		name_box_dja = soup.find('h1', attrs={'class':'name'})
-		price_box_dja = soup.find('div', attrs={'class':'price'})
-		change_box_dja = soup.find('div', attrs={'class':'change-container'})
+		name_box_dja = soup.find('div', {'id':'sectionTitle'})
+		price_box_dja = soup.find('div', {'class':'dataHeader'})
+		#change_box_dja = soup.find('div', attrs={'class':'change-container'})
 
 		
-		await client.say(name_box_dja.text.strip())
-		await client.say('$' + price_box_dja.text)
-		await client.say(change_box_dja.text.strip())
-		await client.say(quote_page_dja)
+		await self.send(name_box_dja.text.strip())
+		await self.send('$' + price_box_dja.text.strip())
+		#await self.send(change_box_dja.text.strip())
+		#await self.send(quote_page_dja)
 
 	else:
-		quote_page = ('https://www.bloomberg.com/quote/%s:US' % symbol)
+		quote_page = ('https://www.reuters.com/finance/stocks/overview/%s' % symbol)
 		page = urllib.request.urlopen(quote_page)
 		soup = BeautifulSoup(page, 'html.parser')
 
-		name_box = soup.find('h1', class_ ='companyName__99a4824b')
-		price_box = soup.find('span', 'priceText__1853e8a5')
-		percent_up_down = soup.find('span', class_ = 'changePercent__2d7dc0d2')
-		change_box = soup.find('span', class_ = 'changeAbsolute__395487f7')
+		name_box = soup.find('div', {'id': 'sectionTitle'})
+		price_box = soup.find('br', attrs = {'class' : 'clear'})
+		print("im in the else statement now")
+		print(quote_page)
+		price_box = price_box.find_next_sibling("span").get_text()
+		#percent_up_down = soup.find('span', class_ = 'changePercent__2d7dc0d2')
+		#change_box = soup.find('span', class_ = 'changeAbsolute__395487f7')
 
-		
-		await client.say(name_box.text)
-		await client.say('$' + price_box.text)
-		await client.say(change_box.text + " " + percent_up_down.text)
-		await client.say(quote_page)
+		name = name_box.text.strip()
+		await self.send(name)
+		price = price_box.strip()
+		await self.send('$' + price)
+		#await symbol.send(change_box.text + " " + percent_up_down.text)
+		#await symbol.send(quote_page)
 
 async def list_servers():
 	await client.wait_until_ready()

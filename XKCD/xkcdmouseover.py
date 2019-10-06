89136bd746b494ba611eec1afb5ab32a040a1e53
Gam8it <32150386+Gam8itG4mes@users.noreply.github.com>
2019-10-06 16:49:05 -0700

    Updated with file read/write and random capabilities

--- XKCD/xkcdmouseover.py
+++ XKCD/xkcdmouseover.py
@@ -1,16 +1,42 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import random, time
 
-url = 'https://www.xkcd.com/2001'
 
-comic_page = urllib.request.urlopen(url);
 
-soup = BeautifulSoup(comic_page, 'html.parser')
+f = open('prev_comics.txt', 'a')
+prev_numbers = set(line.rstrip() for line in open("prev_comics.txt"));
 
-comic_block = soup.findAll('img')
+#for i in prev_numbers:
+#	print(i);
 
-#html_soup = BeautifulSoup(comic_block, 'html.parser')
-#html_soup.find('img')
+counter = 0;
+while counter < 5:
+	
+	x = random.randint(800, 805);
 
-print(comic_block[2].get('title'))
+	url = ('https://www.xkcd.com/%d' % x);
+	comic_page = urllib.request.urlopen(url);
+	soup = BeautifulSoup(comic_page, 'html.parser')
+	comic_block = soup.findAll('img')
+	
+
+	#for line in open('prev_comics.txt'):
+	if x in prev_numbers:
+		print("%d already in set" % x);
+	elif x not in prev_numbers:
+		f.write('%d\n' % x);
+		prev_numbers.add(x);
+		print(x);
+		print(comic_block[2].get('title'));
+		
+	#f.write('%d\n' % x);
+
+	
+	
+
+	counter += 1;
+	time.sleep(30);
 #print(html_soup)
+
+
